b7b677c0bb5ab4214cd333287c68ba95aeb59711
Emil Terman <terman.emil@gmail.com>
2018-04-19 16:43:19 +0300

    I added the borders

--- lab5/WindowsFormsAPP_TanksGame/Tanks/Tanks/Form1.cs
+++ lab5/WindowsFormsAPP_TanksGame/Tanks/Tanks/Form1.cs
@@ -37,8 +37,8 @@ namespace Tanks
 
             var tank = GameObject.Instantiate(new Tank(
                 GameController.rootPath + "imgs\\TankPlayer.png",
-                new PointF(0.01f, 0.01f),
-                false));
+                speed: new PointF(0.05f, 0.05f),
+                isAI: false));
 
             var tank2 = GameObject.Instantiate(new Tank(
                 GameController.rootPath + "imgs\\TankEnemy.png",
@@ -48,21 +48,21 @@ namespace Tanks
             tank2.transform.position = new PointF(100, 100);
 
             GameObject.Instantiate(new Border(
-                new PointF(50, 10),
-                new PointF(70, 220)));
-            /*
-            new Border(
-                new PointF(0, 0),
-                new PointF(-10, mainDrawContext.Height));
+                new PointF(0, -100),
+                new PointF(mainDrawContext.Width, 0)));
 
-            new Border(
+            GameObject.Instantiate(new Border(
+                new PointF(-100, 0),
+                new PointF(0, mainDrawContext.Height)));
+
+            GameObject.Instantiate(new Border(
                 new PointF(mainDrawContext.Width, 0),
-                new PointF(mainDrawContext.Width, mainDrawContext.Height + 10));
+                new PointF(mainDrawContext.Width, mainDrawContext.Height + 10)));
 
-            new Border(
+            GameObject.Instantiate(new Border(
                 new PointF(0, mainDrawContext.Height),
-                new PointF(mainDrawContext.Width + 1, mainDrawContext.Height));
-               */
+                new PointF(mainDrawContext.Width + 1, mainDrawContext.Height)));
+                
             timer1.Start();
         }
 
